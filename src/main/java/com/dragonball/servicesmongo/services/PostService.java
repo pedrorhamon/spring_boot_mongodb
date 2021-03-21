81d8618178fd045a2a43f301a1b354c840223f81
Pedro <pedrorhamon16@gmail.com>
2021-03-20 21:18:44 -0300

    Acrescentando comentários aos posts

---
+++ src/main/java/com/dragonball/servicesmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.dragonball.servicesmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dragonball.servicesmongo.domain.Post;
+import com.dragonball.servicesmongo.repository.PostRepository;
+import com.dragonball.servicesmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
