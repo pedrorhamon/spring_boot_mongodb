9e25195efa44955b3715749de744cdf68592575d
Pedro <pedrorhamon16@gmail.com>
2021-03-22 10:43:14 -0300

    Consulta simples com query methods

--- src/main/java/com/dragonball/servicesmongo/repository/PostRepository.java
+++ src/main/java/com/dragonball/servicesmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.dragonball.servicesmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.dragonball.servicesmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContaining(String text);
 
 }
