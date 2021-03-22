bcfa725301a5f6080cfb47704379b8312974ff6e
Pedro <pedrorhamon16@gmail.com>
2021-03-22 11:08:39 -0300

    Consulta simples com Query

--- src/main/java/com/dragonball/servicesmongo/repository/PostRepository.java
+++ src/main/java/com/dragonball/servicesmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.dragonball.servicesmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.dragonball.servicesmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'search': { $regex: ?0, $options: ''} }")
+	List<Post> search(String text);
 	
 	List<Post> findByTitleContaining(String text);
 
--- src/main/java/com/dragonball/servicesmongo/services/PostService.java
+++ src/main/java/com/dragonball/servicesmongo/services/PostService.java
@@ -22,6 +22,6 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text){
-		return repo.findByTitleContaining(text);
+		return repo.search(text);
 	}
 }
